029cb9775d9819e9c4fa1ca4b1a85094af70658c
Anderson <andersonsantofarias@outlook.com>
2018-09-17 22:51:56 -0300

    Alterando inteface e relacionamento entre tabelas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :ingredients
-  resources :dishes
   resources :restaurants
+  resources :dishes do
+    collection do
+      get 'homepage'
+    end
+  end
+
+  root 'dishes#homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
