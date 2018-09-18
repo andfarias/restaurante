e8867041bb6362dc279c8e75df3bde8f4d3a4cb6
Anderson <andersonsantofarias@outlook.com>
2018-09-18 20:02:35 -0300

    Criando pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'dishes#homepage'
+  root 'dishes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
